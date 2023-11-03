94cf9765cc8804ce1776cf97099c30db872054bc
Hydra-Bolt <amubashir200018@gmail.com>
2023-11-03 12:39:22 +0500

    fixed big bug

--- facedata.py
+++ facedata.py
@@ -1,47 +1,74 @@
-import threading
-
 import cv2
-from deepface import DeepFace
+import os
+import tkinter as tk
+from tkinter import Entry, Button
+import winsound
 
-def checkFace(frame):
-    global face_match
-    try:
-        if DeepFace.verify(frame, rfrnce.copy())['verified']:
-            face_match = True
+click_sound_path = "click.wav"
+
+def play_click_sound():
+    winsound.PlaySound(click_sound_path, winsound.SND_FILENAME)
+
+def capture_images(name):
+    # Create a VideoCapture object to access the camera
+    cap = cv2.VideoCapture(0)
+    os.mkdir(f"./faces/{name}")
+    # Initialize the font and text parameters
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_color = (0, 0, 255)  # Red color
+    font_thickness = 2
+
+    facial_dim = ["Front", "Side-Left", "Side-Right", "Down", "Up"]
+    for face in facial_dim:
+        countdown_duration = 5
+        while countdown_duration > 0:
+            ret, frame = cap.read()
+
+            if not ret:
+                print("Error: Could not read a frame from the camera.")
+                break
+
+            # Display the countdown on the image
+            countdown_text = f"Capturing {face} in {countdown_duration}"
+            text_size, _ = cv2.getTextSize(countdown_text, font, font_scale, font_thickness)
+            text_x = (frame.shape[1] - text_size[0]) // 2
+            text_y = (frame.shape[0] + text_size[1]) // 2
+            cv2.putText(frame, countdown_text, (text_x, text_y), font, font_scale, font_color, font_thickness)
+
+            cv2.imshow("Countdown Timer", frame)
+            cv2.waitKey(1000)  # Delay for 1 second
+            countdown_duration -= 1
+        play_click_sound()
+        # Capture an image after the countdown
+        ret, image = cap.read()
+
+        if ret:
+            cv2.imwrite(f"./faces/{name}/{name}_{face}.png", image)
+            print(f"Image captured and saved as {name}_{face}.png")
         else:
-            face_match = False
-    except ValueError:
-        face_match = False
+            print("Error: Could not capture an image.")
 
+    # Release the camera and close OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+def submit_name():
+    name = name_entry.get()
+    capture_images(name)
+    window.destroy()
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Create the GUI window
+window = tk.Tk()
+window.title("Face Image Capture")
+window.geometry(800,800)
+# Create and place GUI elements
+name_label = tk.Label(window, text="Enter your name:")
+name_label.pack()
+name_entry = Entry(window)
+name_entry.pack()
+submit_button = Button(window, text="Submit", command=submit_name)
+submit_button.pack()
 
-count = 0
-
-match = False
-
-rfrnce = cv2.imread("reference.jpg")
-
-while True:
-    ret, frame = cap.read()
-    
-    if ret:
-        if count%30 == 0:
-            try:
-                threading.Thread(target = checkFace, args = (frame.copy(),)).start()
-            except ValueError:
-                pass
-        count+=1
-        
-        if match:
-            
-            cv2.putText(frame, "Match!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-        else:
-            cv2.putText(frame, "No Match!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-    key= cv2.waitKey(1)
-    if key == ord("q"):
-        break
-cv2.destroyAllWindows()
+# Start the GUI main loop
+window.mainloop()
---
+++ facerecog.py
@@ -0,0 +1,72 @@
+import threading
+import os
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cascPath = "haarcascade_frontalcatface.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+faces_dict = {face: os.listdir(f"./faces/{face}") for face in os.listdir("./faces")}
+face_match = False
+confidence = 0
+matched_name: str = ""
+
+
+def check_face(frame):
+    global face_match, matched_name, confidence
+    for name, face_angles in faces_dict.items():
+        for face_angle in face_angles:
+            # print(f"./faces/{name}/{face_angle}")
+            reference_img = cv2.imread(f"./faces/{name}/{face_angle}")
+            result  = DeepFace.verify(frame, reference_img.copy())
+            try:
+                if result["verified"]:
+                    face_match = True
+                    matched_name = name
+                    confidence = result["distance"]
+                    return
+                else:
+                    face_match = False
+                    matched_name = ""
+            except ValueError:
+                face_match = False
+                matched_name = ""
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                check_face(frame.copy())
+            except ValueError:
+                pass
+        counter += 1
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags= cv2.CASCADE_SCALE_IMAGE
+        )
+        for (x, y, w, h) in faces:
+            # print(matched_name)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(frame, matched_name if matched_name else "Not Recognized" + ":" + str(confidence), (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
